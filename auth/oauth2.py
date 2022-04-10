0668be0a7a81c5f4ef8ba033ef3d669bfee94827
Taha <t.shakibania@mybitmax.com>
2022-04-10 15:10:06 +0430

    feat: section 9 (authentication) completed

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -1,7 +1,11 @@
 from datetime import timedelta, datetime
+from fastapi import Depends, HTTPException, status
 from typing import Optional
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
+from jose import jwt, JWTError
+from sqlalchemy.orm.session import Session
+from db.database import get_db
+from db import db_user
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
@@ -20,3 +24,25 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    authentication_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not verify credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        if not username:
+            raise authentication_exception
+        
+    except JWTError:
+        raise authentication_exception
+    
+    user = db_user.get_by_username(db, username)
+    return user
