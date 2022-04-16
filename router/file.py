a2fbcbd7ed4a6d03a01017233aaf0787e453fd2b
Taha <t.shakibania@mybitmax.com>
2022-04-16 12:49:19 +0430

    feat: section 10, episode 3 complete

--- router/file.py
+++ router/file.py
@@ -1,11 +1,21 @@
-from fastapi import APIRouter, File
+from fastapi import APIRouter, File, UploadFile
+import shutil
 
 
 router = APIRouter(prefix="/file", tags=["file"])
 
 
-@router.post("/upload")
-def upload_file(file: bytes = File(...)):
-    content = file.decode('utf-8')
-    lines = content.split('\n')
-    return {'lines': lines}
+@router.post("/file")
+def get_file(file: bytes = File(...)):
+    content = file.decode("utf-8")
+    lines = content.split("\n")
+    return {"lines": lines}
+
+
+@router.post("/uploadfile")
+def get_uploadfile(uploadfile: UploadFile = File(...)):
+    path = f"files/{uploadfile.filename}"
+    with open(path, "w+b") as buffer:
+        shutil.copyfileobj(uploadfile.file, buffer)
+
+    return {"address": path, "file_type": uploadfile.content_type}
