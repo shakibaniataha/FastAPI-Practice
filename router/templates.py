280eab5d79e57f934b7916021fa819b65b101738
Taha <t.shakibania@mybitmax.com>
2022-04-24 14:08:09 +0430

    feat: section 12, episode 5 complete

--- router/templates.py
+++ router/templates.py
@@ -2,19 +2,23 @@ from fastapi.requests import Request
 from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-
+from fastapi import BackgroundTasks
 from schemas import ProductBase
 
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory="templates")
 
 
-router = APIRouter(prefix='/templates', tags=['templates'])
+router = APIRouter(prefix="/templates", tags=["templates"])
+
+@router.post("/product/{id}", response_class=HTMLResponse)
+def get_product_info_template(
+    id: int, request: Request, product: ProductBase, bt: BackgroundTasks
+):
+    bt.add_task(print_request, "This is a sample log message")
+    return templates.TemplateResponse(
+        "product.html", {"request": request, "id": id, "product": product}
+    )
 
 
-@router.post('/product/{id}', response_class=HTMLResponse)
-def get_product_info_template(id: int, request: Request, product: ProductBase):
-    return templates.TemplateResponse('product.html', {
-        "request": request,
-        "id": id, 
-        "product": product
-    })
+def print_request(message: str):
+    print(message)
